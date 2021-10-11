46c8387a71b21f66f2d15fbbe33db1afe2232fb1
DESKTOP-ZOTIN\Serguei <szotin@gmail.com>
2021-10-11 13:50:48 -0700

    Applied some of the db changes in the main brunch, so file is split now.

---
+++ db/alter122.sql
@@ -0,0 +1,16 @@
+DROP TABLE rebuild_ratings;
+
+CREATE TABLE `rebuild_ratings` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`start_time` INT(11) NOT NULL,
+	`end_time` INT(11) NOT NULL,
+	`game_id` INT(11) NOT NULL,
+
+	PRIMARY KEY (`id`),
+	KEY `game` (`game_id`),
+	CONSTRAINT `game_fk` FOREIGN KEY (`game_id`) REFERENCES `games` (`id`),
+	UNIQUE INDEX `start` (`start_time`),
+	UNIQUE INDEX `end` (`end_time`)
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
