94ff9310b35ba4fa59363b6b26613850242003e9
Oleg Kulygin <okulygin@gmail.com>
2021-12-17 19:55:58 -0800

    hide roles when arranging cards

--- plugins/obs/PlayersOverlayPlugin/src/app/components/player/player.component.ts
+++ plugins/obs/PlayersOverlayPlugin/src/app/components/player/player.component.ts
@@ -9,7 +9,10 @@ import { Game, GamePhase, GameState, Player, PlayerRole } from 'src/app/services
 export class PlayerComponent implements OnInit {
   @Input() player!: Player;
   @Input() game!: Game | undefined;
+
   showRoles: boolean = false;
+
+  private isDayOccured: boolean = false;
 
   constructor() { }
 
@@ -17,16 +20,30 @@ export class PlayerComponent implements OnInit {
   }
 
   ngOnChanges(changes: SimpleChanges) {
-    console.log(changes);
     // changes.prop contains the old and the new value...
     let player: Player = changes['player'].currentValue;
     if (player.id === 0) {
       player.role = PlayerRole.none;
     }
+
     let game: Game = changes['game'].currentValue;
 
-    this.showRoles = (game && game.state != GameState.notStarted && (game.state !== "starting" || (game.round > 0 && game.phase !== GamePhase.night))) ?? false;
+    // BUG mitigation (remove when fixed) - Remember if we had a day phase to distinguish between 2 states with same API states (moderator selecting roles and blank screen before 1st night)
+    if (game.state === GameState.notStarted) {
+      this.isDayOccured = false;
+    } else {
+      this.isDayOccured = this.isDayOccured || game.phase === GamePhase.day;
+    }
+
+    this.showRoles = (
+      game
+      && game.state != GameState.notStarted 
+      && (game.state !== GameState.starting
+         || (game.phase === GamePhase.night && (game.round > 0 || this.isDayOccured))// "starting"
+         || (game.phase === GamePhase.day && game.round >= 0)
+      )) ?? false;
+
+    console.log(changes);
     console.log(this.showRoles);
   }
-
 }
