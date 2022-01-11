6511b9ff4ddfe65acb7c4b80fffca79b4de7af95
Oleg Kulygin <okulygin@gmail.com>
2022-01-10 17:51:48 -0800

    fix bug in obs i18n resource loading

--- plugins/obs/PlayersOverlayPlugin/src/app/app.module.ts
+++ plugins/obs/PlayersOverlayPlugin/src/app/app.module.ts
@@ -42,5 +42,5 @@ export class AppModule { }
 
 // required for AOT compilation
 export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
-  return new TranslateHttpLoader(http);
+  return new TranslateHttpLoader(http, './assets/i18n/');
 }
