63575262b0090c2260ea0a0b30d9db966e687a2c
Oleg Kulygin <okulygin@gmail.com>
2022-01-10 17:36:57 -0800

    load current language from the "locale" URL parameter

--- plugins/obs/PlayersOverlayPlugin/src/app/app.component.ts
+++ plugins/obs/PlayersOverlayPlugin/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import {TranslateService} from '@ngx-translate/core';
+import { ActivatedRoute } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,19 @@ import {TranslateService} from '@ngx-translate/core';
 export class AppComponent {
   title = 'PlayersOverlayPlugin';
 
-  constructor(private translate: TranslateService) {
+  constructor(private translate: TranslateService, private activatedRoute: ActivatedRoute) {
     translate.setDefaultLang('en');
+
+    this.activatedRoute.queryParams.subscribe((params: { [x: string]: any; }) => {
+      this.processUrlParams(params);
+    });
+  }
+
+  private processUrlParams(params: { [x: string]: any; }) {
+    const locale = params['locale'];
+
+    if (locale) {
+      this.translate.use(locale);
+    }
   }
 }
