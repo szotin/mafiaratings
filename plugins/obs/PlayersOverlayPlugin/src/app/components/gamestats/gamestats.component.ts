88d1f47929b3544dd005c0cc819af9fb2a4561d9
Oleg Kulygin <okulygin@gmail.com>
2022-01-13 16:11:59 -0800

    add hide-roles flag support for gamestats component

--- plugins/obs/PlayersOverlayPlugin/src/app/components/gamestats/gamestats.component.ts
+++ plugins/obs/PlayersOverlayPlugin/src/app/components/gamestats/gamestats.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Player } from 'src/app/services/gamesnapshot.model';
 import { GamesnapshotService } from 'src/app/services/gamesnapshot.service';
+import { UrlParametersService } from 'src/app/services/url-parameters.service';
+
 import { Observable } from 'rxjs';
 
 @Component({
@@ -12,9 +14,14 @@ export class GamestatsComponent implements OnInit {
   nominees$?: Observable<Player[]>;
   checkedBySheriff$?: Observable<Player[]>;
   checkedByDon$?: Observable<Player[]>;
+  hideRoles$?: Observable<boolean>;
+
   isOffline$: Observable<boolean>;
 
-  constructor(private gameSnapshotService: GamesnapshotService) {
+  constructor(
+    private gameSnapshotService: GamesnapshotService,
+    private urlParameterService: UrlParametersService) {
+
     this.isOffline$ = this.gameSnapshotService.isOffline$;
   }
 
@@ -22,6 +29,7 @@ export class GamestatsComponent implements OnInit {
     this.nominees$ = this.gameSnapshotService.getNominees();
     this.checkedBySheriff$ = this.gameSnapshotService.getCheckedBySheriff();
     this.checkedByDon$ = this.gameSnapshotService.getCheckedByDon();
+    this.hideRoles$ = this.urlParameterService.getHideRoles$();
   }
 
   trackPlayerById(index: number, player: Player): number {
