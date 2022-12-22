faf7ca8b3cd6ed50f042321497ce22f92d40c9f0
Oleg Kulygin <olkuly@microsoft.com>
2022-12-22 11:33:00 -0800

    convert gamestat to row

--- plugins/obs/PlayersOverlayPlugin/src/app/components/gamestats/gamestats.component.ts
+++ plugins/obs/PlayersOverlayPlugin/src/app/components/gamestats/gamestats.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Player } from 'src/app/services/gamesnapshot.model';
 import { GamesnapshotService } from 'src/app/services/gamesnapshot.service';
 import { UrlParametersService } from 'src/app/services/url-parameters.service';
+import { retry, share, switchMap, pluck, map, catchError, delay, skip, tap, concat, concatWith, concatMap, retryWhen, delayWhen } from 'rxjs/operators';
 
 import { Observable } from 'rxjs';
 
@@ -15,6 +16,9 @@ export class GamestatsComponent implements OnInit {
   legacyPlayers$?: Observable<Player[]>;
   checkedBySheriff$?: Observable<Player[]>;
   checkedByDon$?: Observable<Player[]>;
+
+  isLegacyPlayers$?: Observable<boolean>;
+
   hideRoles$?: Observable<boolean>;
 
   isOffline$: Observable<boolean>;
@@ -31,6 +35,13 @@ export class GamestatsComponent implements OnInit {
     this.checkedBySheriff$ = this.gameSnapshotService.getCheckedBySheriff();
     this.checkedByDon$ = this.gameSnapshotService.getCheckedByDon();
     this.legacyPlayers$ = this.gameSnapshotService.getLegacyPlayers();
+
+    this.isLegacyPlayers$ = this.legacyPlayers$.pipe(
+      map((it: Player[])=> {
+        console.log(it?.length);
+        return it.length > 0 ?? false;
+      }));
+    
     this.hideRoles$ = this.urlParameterService.getHideRoles$();
   }
 
