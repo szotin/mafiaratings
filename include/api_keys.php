671ee31c2f2fc603cd5f4f1049db919ae350a5c7
szotin <szotin@gmail.com>
2025-08-19 17:07:40 -0700

    Remove keys from repo

--- include/api_keys.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Never add it to github repository. This is security critical information.
-define('GOOGLE_API_KEY', 'AIzaSyAUuFoIXqzN4c08t13tPX_vW2OZ6c8SA2U');
-define('IMAFIA_API_KEY', 't1923765-af6c-4t6e-4434-323sd7c39326');
-
-?>
